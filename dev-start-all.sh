bf6021e363b1b133619a02f4684c73d60574f32a
kirill.tolkachev <kirill.tolkachev@jugru.org>
2022-04-04 12:39:31 +0300

    chore: fix auto run for sdk-shared subproject

--- dev-start-all.sh
+++ dev-start-all.sh
@@ -4,10 +4,12 @@ WORK_DIR=$(dirname "$(realpath -s "$0")")
 SESSION_NAME="company development env"
 SESSION_NAME_UI="${SESSION_NAME}:1.1"
 SESSION_NAME_SDK_BUILD="${SESSION_NAME}:1.2"
+SESSION_NAME_SDK_SHARED_BUILD="${SESSION_NAME}:1.3"
 echo -e "Init session with:\n"
 echo -e "  Working dir     : ${WORK_DIR}"
-echo -e "  UI session      : ${SESSION_NAME_UI}"
-echo -e "  SDK session     : ${SESSION_NAME_SDK_BUILD}"
+echo -e "  UI              : ${SESSION_NAME_UI}"
+echo -e "  SDK             : ${SESSION_NAME_SDK_BUILD}"
+echo -e "  SDK build       : ${SESSION_NAME_SDK_SHARED_BUILD}"
 echo ""
 
 tmux kill-session -t "${SESSION_NAME}"
@@ -16,7 +18,12 @@ tmux new-session -d -s "${SESSION_NAME}"
 tmux split-window -h -t "${SESSION_NAME_UI}"
 tmux split-window -v -t "${SESSION_NAME_SDK_BUILD}"
 
-tmux send-keys -t "${SESSION_NAME_SDK_BUILD}" "cd $WORK_DIR && npm run sdk:build && npm run sdk:dev" Enter
+# Prepare to good run
+npm run sdk-shared:build
+npm run sdk:build
+
+tmux send-keys -t "${SESSION_NAME_SDK_SHARED_BUILD}" "cd $WORK_DIR && npm run sdk-shared:dev" Enter
+tmux send-keys -t "${SESSION_NAME_SDK_BUILD}" "cd $WORK_DIR && npm run sdk:dev" Enter
 tmux send-keys -t "${SESSION_NAME_UI}" "cd $WORK_DIR && npm run ui:dev" Enter
 
 tmux attach-session -t "${SESSION_NAME}"
