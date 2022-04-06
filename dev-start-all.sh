289d931b3893e7113f2c1b2ff46cf9f5d3eb91c5
kirill.tolkachev <kirill.tolkachev@jugru.org>
2022-04-06 14:24:45 +0300

    chore: fix tmux start

--- dev-start-all.sh
+++ dev-start-all.sh
@@ -16,7 +16,7 @@ tmux new-session -d -s "${SESSION_NAME}"
 tmux split-window -h -t "${SESSION_NAME_UI}"
 tmux split-window -v -t "${SESSION_NAME_SDK_BUILD}"
 
-tmux send-keys -t "${SESSION_NAME_SDK_BUILD}" "cd $WORK_DIR && npm run sdk:build && npm run sdk:dev" Enter
+tmux send-keys -t "${SESSION_NAME_SDK_BUILD}" "cd $WORK_DIR && npm run sdk:dev" Enter
 tmux send-keys -t "${SESSION_NAME_UI}" "cd $WORK_DIR && npm run ui:dev" Enter
 
 tmux attach-session -t "${SESSION_NAME}"
